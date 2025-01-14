13f01fb92b96073860c2555d67591466541f17ea
kesava <kesava.kumar@zohocorp.com>
2025-01-15 00:06:20 +0530

    Initial commit

---
+++ app/src/main/java/com/kesavakumar/echojournal/ui/theme/Color.kt
@@ -0,0 +1,93 @@
+package com.kesavakumar.echojournal.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFF00419C) //
+val onPrimaryLight = Color(0xFFFFFFFF)//
+val primaryContainerLight = Color(0xFF1F70F5)//
+//val onPrimaryContainerLight = Color(0xFF002366)
+val onPrimaryContainerLight = Color(0xFF001945)
+
+// Secondary Colors
+val secondaryLight = Color(0xFF3B4663)//
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFF6B7796)//
+//val onSecondaryContainerLight = Color(0xFF1A1A1A)
+val onSecondaryContainerLight = Color(0xFF001946)
+
+//val tertiaryLight = Color(0xFF5D5D68)
+//val onTertiaryLight = Color(0xFFFFFFFF)
+//val tertiaryContainerLight = Color(0xFFF6F4FF)
+//val onTertiaryContainerLight = Color(0xFF52525C)
+
+val tertiaryLight = Color(0xFF475D92)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFD9E2FF)
+val onTertiaryContainerLight = Color(0xFF001945)
+
+val errorLight = Color(0xFFBA1A1A)
+//val errorLight = Color(0xFF904A4B)
+val onErrorLight = Color(0xFFFFFFFF) //
+val errorContainerLight = Color(0xFFFFEDEC) //
+val onErrorContainerLight = Color(0xFF680014)  //
+
+val backgroundLight = Color(0xFFFCFDFE) //
+//val onBackgroundLight = Color(0xFFFFFFFF)
+val onBackgroundLight = Color(0xFF1A1B20)
+
+
+val surfaceLight = Color(0xFFFFFFFF) //
+val onSurfaceLight = Color(0xFF191A20) //
+val surfaceVariantLight = Color(0xFFE1E2EC) //
+val onSurfaceVariantLight = Color(0xFF40434F) //
+
+val outlineLight = Color(0xFF6C7085)  //
+val outlineVariantLight = Color(0xFFC1C3CE) //
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF313030)
+val inverseOnSurfaceLight = Color(0xFFEEF0FF) //
+val inversePrimaryLight = Color(0xFFBAC6E9) //
+val surfaceDimLight = Color(0xFFDDD9D9)
+val surfaceBrightLight = Color(0xFFFCF8F8)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFF6F3F2)
+val surfaceContainerLight = Color(0xFFF1EDEC)
+val surfaceContainerHighLight = Color(0xFFEBE7E7)
+val surfaceContainerHighestLight = Color(0xFFE5E2E1)
+
+
+val primaryDark = Color(0xFFB0C6FF)
+val onPrimaryDark = Color(0xFF162E60)
+val primaryContainerDark = Color(0xFF2F4578)
+val onPrimaryContainerDark = Color(0xFFD9E2FF)
+val secondaryDark = Color(0xFFB1C5FF)
+val onSecondaryDark = Color(0xFF162E60)
+val secondaryContainerDark = Color(0xFF2F4578)
+val onSecondaryContainerDark = Color(0xFFDAE2FF)
+val tertiaryDark = Color(0xFFB0C6FF)
+val onTertiaryDark = Color(0xFF152E60)
+val tertiaryContainerDark = Color(0xFF2F4578)
+val onTertiaryContainerDark = Color(0xFFD9E2FF)
+val errorDark = Color(0xFFFFB3B2)
+val onErrorDark = Color(0xFF561D20)
+val errorContainerDark = Color(0xFF733335)
+val onErrorContainerDark = Color(0xFFFFDAD9)
+val backgroundDark = Color(0xFF121318)
+val onBackgroundDark = Color(0xFFE2E2E9)
+val surfaceDark = Color(0xFF0E1415)
+val onSurfaceDark = Color(0xFFDEE3E5)
+val surfaceVariantDark = Color(0xFF44474F)
+val onSurfaceVariantDark = Color(0xFFC5C6D0)
+val outlineDark = Color(0xFF8E9099)
+val outlineVariantDark = Color(0xFF44474F)
+val scrimDark = Color(0xFF000000)
+val inverseSurfaceDark = Color(0xFFDEE3E5)
+val inverseOnSurfaceDark = Color(0xFF2B3133)
+val inversePrimaryDark = Color(0xFF475D92)
+val surfaceDimDark = Color(0xFF0E1415)
+val surfaceBrightDark = Color(0xFF343A3B)
+val surfaceContainerLowestDark = Color(0xFF090F10)
+val surfaceContainerLowDark = Color(0xFF171D1E)
+val surfaceContainerDark = Color(0xFF1B2122)
+val surfaceContainerHighDark = Color(0xFF252B2C)
+val surfaceContainerHighestDark = Color(0xFF303637)
